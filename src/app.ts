fea21a5081b9e953b87396af67a89a8cedd7f1a2
Bocket-ChatBot <ceo@bocket.co>
2025-09-04 21:07:50 -0500

    2025-09-04 03

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: { 
         // args: ['--proxy-server=proxy-server-that-requires-authentication.example.com'],
-        headless: false,
+        headless: true,
         executablePath: '/usr/bin/chromium',
     },
 });
@@ -356,64 +356,6 @@ client.on('message_create', async (msg) => {
             console.log(result); // True if the operation completed successfully, false otherwise
         }
     }
-});
-
-client.on('message_ciphertext', (msg) => {
-    // Receiving new incoming messages that have been encrypted
-    // msg.type === 'ciphertext'
-    msg.body = 'Waiting for this message. Check your phone.';
-    
-    // do stuff here
-});
-
-client.on('message_revoke_everyone', async (after, before) => {
-    // Fired whenever a message is deleted by anyone (including you)
-    console.log(after); // message after it was deleted.
-    if (before) {
-        console.log(before); // message before it was deleted.
-    }
-});
-
-client.on('message_revoke_me', async (msg) => {
-    // Fired whenever a message is only deleted in your own view.
-    console.log(msg.body); // message before it was deleted.
-});
-
-client.on('message_ack', (msg, ack) => {
-    /*
-        == ACK VALUES ==
-        ACK_ERROR: -1
-        ACK_PENDING: 0
-        ACK_SERVER: 1
-        ACK_DEVICE: 2
-        ACK_READ: 3
-        ACK_PLAYED: 4
-    */
-
-    if (ack == 3) {
-        // The message was read
-    }
-});
-
-client.on('group_join', (notification) => {
-    // User has joined or been added to the group.
-    console.log('join', notification);
-    notification.reply('User joined.');
-});
-
-client.on('group_leave', (notification) => {
-    // User has left or been kicked from the group.
-    console.log('leave', notification);
-    notification.reply('User left.');
-});
-
-client.on('group_update', (notification) => {
-    // Group picture, subject or description has been updated.
-    console.log('update', notification);
-});
-
-client.on('change_state', state => {
-    console.log('CHANGE STATE', state);
 });
 
 // Change to false if you don't want to reject incoming calls
